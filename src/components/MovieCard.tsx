1cf9d82bb27973db48c1731e06fde0706d009596
a13xand9r <a13x.student@yandex.ru>
2021-09-28 15:25:04 +0300

    movie card changes

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,12 +1,8 @@
-import { IconStarFill, IconCalendar, IconAccessibility, IconEvent } from '@sberdevices/plasma-icons'
-import { Card, CardBody, CardContent, TextBox, Image, TextBoxBigTitle, Cell, CellIcon, TextBoxSubTitle, CardMedia } from '@sberdevices/plasma-ui'
+import { IconStarFill, IconAccessibility, IconEvent } from '@sberdevices/plasma-icons'
+import { Card, CardBody, CardContent, TextBox, TextBoxBigTitle, Cell, CellIcon, TextBoxSubTitle, CardMedia } from '@sberdevices/plasma-ui'
 import styled from 'styled-components'
 import { TVShowType } from '../types'
 
-const StyledImage = styled(Image)`
-  max-width: 400px;
-  margin: 0 auto;
-`
 const StyledTextBox = styled(TextBox)`
   margin-top: 1rem;
   /* margin-bottom: 4rem; */
@@ -15,53 +11,45 @@ const StyledTextBox = styled(TextBox)`
 export const MovieCard = ({ movie }: { movie: TVShowType }) => {
 
   return (
-    <div>
-      {/* <StyledImage
-        src={movie.img}
-      // style={{ width: "330px", height: "550px", marginBottom: "5px" }}
-      /> */}
-      {/* <img src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${movie.img}`} alt="" /> */}
-      <Card>
-        <CardBody>
-          <CardMedia
-            src={movie.img}
-            placeholder="/images/320_320_1.jpg"
-            ratio="9 / 16"
-            style={{paddingBottom: '900px'}}
+    <Card>
+      <CardBody>
+        <CardMedia
+          src={movie.img}
+          placeholder="/images/320_320_1.jpg"
+          ratio="9 / 16"
+          style={{ paddingBottom: '900px' }}
+        />
+        <CardContent style={{ height: '100%', padding: '1.5rem' }} cover={false}>
+          <TextBox>
+            <TextBoxBigTitle>{movie.name}</TextBoxBigTitle>
+          </TextBox>
+          <Cell
+            contentLeft={<CellIcon>
+              <IconStarFill />
+            </CellIcon>}
+            content={<TextBox><TextBoxSubTitle>Оценка: </TextBoxSubTitle><TextBoxSubTitle>{movie.rate}</TextBoxSubTitle></TextBox>}
+            alignRight="center"
           />
-          <CardContent style={{ height: '100%', padding: '1.5rem' }} cover={false}>
-            <TextBox>
-              <TextBoxBigTitle>{movie.name}</TextBoxBigTitle>
-            </TextBox>
-            <Cell
-              contentLeft={<CellIcon>
-                <IconStarFill />
-              </CellIcon>}
-              content={<TextBox><TextBoxSubTitle>Оценка: </TextBoxSubTitle><TextBoxSubTitle>{movie.rate}</TextBoxSubTitle></TextBox>}
-              alignRight="center"
-            />
-            <Cell
-              contentLeft={<CellIcon>
-                <IconEvent />
-              </CellIcon>}
-              content={<TextBox><TextBoxSubTitle>Год: </TextBoxSubTitle><TextBoxSubTitle>{movie.year}</TextBoxSubTitle></TextBox>}
-              alignRight="center"
-            />
-            <Cell
-              contentLeft={<CellIcon>
-                <IconAccessibility />
-              </CellIcon>}
-              content={<TextBox><TextBoxSubTitle>Жанр: </TextBoxSubTitle><TextBoxSubTitle>{movie.genres}</TextBoxSubTitle></TextBox>}
-              alignRight="center"
-            />
-            <StyledTextBox>
-              <TextBoxBigTitle>Описание</TextBoxBigTitle>
-              <TextBoxSubTitle>«{movie.description}»</TextBoxSubTitle>
-            </StyledTextBox>
-          </CardContent>
-        </CardBody>
-      </Card>
-      
-    </div>
+          <Cell
+            contentLeft={<CellIcon>
+              <IconEvent />
+            </CellIcon>}
+            content={<TextBox><TextBoxSubTitle>Год: </TextBoxSubTitle><TextBoxSubTitle>{movie.year}</TextBoxSubTitle></TextBox>}
+            alignRight="center"
+          />
+          <Cell
+            contentLeft={<CellIcon>
+              <IconAccessibility />
+            </CellIcon>}
+            content={<TextBox><TextBoxSubTitle>Жанр: </TextBoxSubTitle><TextBoxSubTitle>{movie.genres.join(', ')}</TextBoxSubTitle></TextBox>}
+            alignRight="center"
+          />
+          <StyledTextBox>
+            <TextBoxBigTitle>Описание</TextBoxBigTitle>
+            <TextBoxSubTitle>{movie.description}</TextBoxSubTitle>
+          </StyledTextBox>
+        </CardContent>
+      </CardBody>
+    </Card>
   )
 }
